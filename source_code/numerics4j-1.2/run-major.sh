f7f5449a31085e7a36cf11757a16e7fbb942b1c8
hessah <hesaah@gmail.com>
2014-10-06 09:16:42 -0600

    update numerics4j with jacoco and major results

---
+++ source_code/numerics4j-1.2/run-major.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# By: Hessah Alkaoud
+
+ant -DmutOp=":ALL" clean compile mutation-test 
+
+time_stamp=$(date +%Y_%m_%d_%H_%M_%S)
+mkdir -p "majorReports/${time_stamp}"
+mv mutants.log killed.csv results.csv "majorReports/${time_stamp}"
+sleep 10;import -window root majorReports/${time_stamp}/${time_stamp}.png
+echo "..............Calculated mutation score using: MAJOR .. Results are stored in: majorReports/ ................"
